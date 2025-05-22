92055d26c4df5c79e19384b3a138d3a9609a12a4
Md.Mazharul Islam <mazharul.saurav255@gmail.com>
2025-05-22 11:37:16 +0600

    content issue solve
    content issue solve

--- MyApp/Controllers/ControllerBase.cs
+++ MyApp/Controllers/ControllerBase.cs
@@ -5,15 +5,19 @@ using Utility;
 
 namespace API.Controllers
 {
-    protected async Task<ActionResult> getResponse(object Data, string Message = "")
+    public class ControllerBase : Controller
     {
-        CommonResponseDto response = new CommonResponseDto();
-        response.Status = "OK";
-        response.Message = Message;
-        response.Data = Data;
+        protected async Task<ActionResult> getResponse(object Data, string Message = "")
+        {
+            CommonResponseDto response = new CommonResponseDto();
+            response.Status = "OK";
+            response.Message = Message;
+            response.Data = Data;
 
-        return Content(new JSONSerialize().getJSONSFromObject(response, true), "application/json");
+            return Content(new JSONSerialize().getJSONSFromObject(response, true), "application/json");
 
-        //return Json(response);
+            //return Json(response);
+        }
+
     }
 }
